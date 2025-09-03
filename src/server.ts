3158f83d01ad1525e65f6081ef0e18a3c3190800
MichaelGetu-git <michaelgetu21@gmail.com>
2025-09-03 18:54:33 +0300

    update: server route setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,26 @@
 import express from 'express';
+import { authenticate } from './middleware/auth';
+import admin from './routes/admin'
+import profile from './routes/profiles'
+import auth from './routes/auth'
+import skills from './routes/skills'
+import companies from './routes/companies'
+import jobs from './routes/jobs'
+import applications from './routes/applications'
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use("/admin", authenticate, admin)
+app.use("/applications", authenticate, applications)
+app.use("/auth", auth)
+app.use("/jobs", authenticate, jobs)
+app.use("/skills", authenticate, skills)
+app.use("/profile",authenticate, profile)
+app.use("/companies", authenticate, companies)
 
 app.listen(port, ()=> {
     console.log(`Port listening on port ${port}`)
